8500866698c2bd5aba224d8aef59d3c080d9db5f
Goulven <goulven.friess@gmail.com>
2023-12-18 05:40:31 -0400

    Update mailgun.js

--- mailgun.js
+++ mailgun.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const nodemailer = require('nodemailer');
 const cors = require("cors");
 const dotenv = require('dotenv');
 const mg = require('mailgun-js');
@@ -14,6 +15,7 @@ const mailgun = () =>
     apiKey: process.env.MAILGUN_API_KEY,
     domain: process.env.MAILGUN_DOMIAN,
   });
+
 
 const app = express();
 app.use(express.json());
@@ -46,6 +48,41 @@ app.post('/api/email', (req, res) => {
         }
       }
     );
+});
+
+app.post('/send-email', async (req, res) => {
+  try {
+    // Paramètres pour la connexion SMTP avec Mailgun
+    const transporter = nodemailer.createTransport({
+      service: 'Mailgun',
+      host: 'smtp.mailgun.org',
+      port: 587,
+      auth: {
+        user: 'postmaster@mg.fillgood.io', // Remplacez par votre nom d'utilisateur SMTP Mailgun
+        pass: '40dd24c5d3ea8dba8da3c649e639f356-07f37fca-28979945', // Remplacez par votre mot de passe SMTP Mailgun
+      },
+    });
+
+    // Récupérer les informations de l'email depuis la requête
+    const { email, subject, message } = req.body;
+
+    // Options de l'email
+    const mailOptions = {
+      from: 'postmaster@mg.fillgood.io', // Remplacez par votre adresse email d'expéditeur
+      email,
+      subject,
+      message,
+    };
+
+    // Envoyer l'email
+    const info = await transporter.sendMail(mailOptions);
+
+    console.log('Email sent: ', info.messageId);
+    res.status(200).json({ success: true, messageId: info.messageId });
+  } catch (error) {
+    console.error('Error sending email: ', error);
+    res.status(500).json({ success: false, error: error.message });
+  }
 });
 
 const port = (process.env.PORT || 4000);
